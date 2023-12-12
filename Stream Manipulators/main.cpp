1c55260da5b387d207ee28471348197dc285a343
RehanGith <chrehan1007@gmail.com>
2023-12-12 17:54:00 +0500

    Boolean formatting flags

--- Stream Manipulators/main.cpp
+++ Stream Manipulators/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-
+	int num{ 200 };
+	cout << (num == 200) << endl;
+	cout << (num == 100) << endl;
+	cout.setf(ios::boolalpha);  // true or false
+	cout << (num == 200) << endl;
+	cout << (num == 100) << endl;
+	cout << resetiosflags(ios::boolalpha);
+	cout << (num == 200) << endl;
+	cout << (num == 100) << endl;
 	return 0;
 }
